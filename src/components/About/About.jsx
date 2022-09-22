6637cbdf92312f9089be03dc95b4e167cdbcfc33
al-jabir <aljabir098@gmail.com>
2022-09-23 03:45:36 +0600

    still working

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import { AiFillFolderOpen, AiOutlineUsergroupAdd } from 'react-icons/ai'
+import { FaAward } from 'react-icons/fa'
+import Me from '../../assets/aj-me2.jpeg'
+import './About.css'
 
 const About = () => {
   return (
     <section id="about">
-      <h3>About</h3>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className="container about_container">
+        <div className="about_me">
+          <div className="about_me-img">
+            <img src={Me} alt="me" />
+          </div>
+        </div>
+        <div className="about_content">
+          <div className="about_cards">
+            <article className="about_card">
+              <FaAward className="about_icon" />
+              <h5>Experience</h5>
+              <small>2 Years Years working</small>
+            </article>
+            <article className="about_card">
+              <AiOutlineUsergroupAdd className="about_icon" />
+              <h5>Clients</h5>
+              <small>150 worldwide</small>
+            </article>
+            <article className="about_card">
+              <AiFillFolderOpen className="about_icon" />
+              <h5>Project</h5>
+              <small>20+ Completed</small>
+            </article>
+          </div>
+          <p>
+            I'm a MERN stack Developer. I can see myself in a top-ranking
+            position in the web developing market. True that this market is
+            growing higher day by day but my skills are also growing as I am a
+            very keen learner and technology lover person. That's why I find
+            this sector very interesting and always work with my full enthusiasm
+          </p>
+        </div>
+      </div>
     </section>
   )
 }
