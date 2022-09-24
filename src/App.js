31f01fcaaa5eacd5f96d26eac029718959195855
al-jabir <aljabir098@gmail.com>
2022-09-25 03:36:01 +0600

    Alhamdulillah I have all done

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import Navbar from './components/Navigation/Navbar'
+import Particle from './components/Particles/Particle'
 import Portfolio from './components/Portfolio/Portfolio'
 import Skill from './components/Skills/Skills'
 
 const App = () => {
   return (
     <>
+      <Particle />
       <Header />
       <Navbar />
       <About />
