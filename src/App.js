55e25fda7ac23ffbde6f9e5f77c407fd246a70f3
al-jabir <aljabir098@gmail.com>
2022-09-24 23:16:34 +0600

    Some update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import About from './components/About/About'
 import Contact from './components/Contact/Contact'
-import Skill from './components/Experience/Skills'
 import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import Navbar from './components/Navigation/Navbar'
 import Portfolio from './components/Portfolio/Portfolio'
 import Service from './components/Service/Service'
+import Skill from './components/Skills/Skills'
 import Testimonials from './components/Testimonials/Testimonials'
 
 const App = () => {
