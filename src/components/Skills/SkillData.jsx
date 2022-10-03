3ea564783fe1540902603136a53a087883034c1c
al-jabir <aljabir098@gmail.com>
2022-10-03 21:29:08 +0600

    skills json done

---
+++ src/components/Skills/SkillData.jsx
@@ -0,0 +1,59 @@
+import css from '../../assets/skills/css.png'
+import firebase from '../../assets/skills/firebase.png'
+import github from '../../assets/skills/github.png'
+import html from '../../assets/skills/html.png'
+import js from '../../assets/skills/javascript.png'
+import mongo from '../../assets/skills/mongo.png'
+import node from '../../assets/skills/node.png'
+import react from '../../assets/skills/react.png'
+import tailwind from '../../assets/skills/tailwind.png'
+
+const skillData = [
+  {
+    id: 1,
+    icon: html,
+    title: 'HTML',
+  },
+  {
+    id: 2,
+    icon: css,
+    title: 'css',
+  },
+  {
+    id: 3,
+    icon: js,
+    title: 'js',
+  },
+  {
+    id: 4,
+    icon: react,
+    title: 'react',
+  },
+  {
+    id: 5,
+    icon: github,
+    title: 'github',
+  },
+  {
+    id: 6,
+    icon: firebase,
+    title: 'firebase',
+  },
+  {
+    id: 7,
+    icon: node,
+    title: 'node',
+  },
+  {
+    id: 8,
+    icon: mongo,
+    title: 'mongo',
+  },
+  {
+    id: 9,
+    icon: tailwind,
+    title: 'tailwind',
+  },
+]
+
+export default skillData
