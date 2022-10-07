e1da6adf36be0adec55ea2f56fedd77f4142e213
al-jabir <aljabir098@gmail.com>
2022-10-07 14:17:47 +0600

    Addded resume

--- src/components/Header/CTA.jsx
+++ src/components/Header/CTA.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import cv from '../../assets/cv/jabir-cv2.pdf'
 
 const CTA = () => {
   return (
     <div className="cta">
-      <a href="/" className="btn">
+      <a href={cv} className="btn">
         Download CV
       </a>
       <a href="#contact" className="btn btn-primary">
